ad822bdcc28febeabdae0919a21a622bd21e758e
Mateusz Bogacz <shay_am@wp.pl>
2021-04-25 20:06:56 +0200

    deleted unused varibale

--- src/views/App.js
+++ src/views/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import AppContext from "../context";
 import Header from "../components/Header/Header";
 import Main from "../components/main/Main";
-import Task from "../components/main/Task/Task";
 
 class App extends React.Component {
   state = {
     arrItem: [],
-    arrItemIsComplete: [],
-
     counter: 0,
   };
 
@@ -16,28 +13,6 @@ class App extends React.Component {
     e.preventDefault();
     this.setState((prevState) => ({
       arrItem: [...(prevState.arrItem || []), item],
-    }));
-  };
-  showComplete = (e) => {
-    // let newArray = [...this.state.arrItem];
-
-    return this.state.arrItem
-      .filter((item) => item.isCompleted === true)
-      .map((item) => <Task key={item.id} {...item} />);
-  };
-
-  clearComplete = () => {
-    this.setState((prevState) => ({
-      ...prevState.arrItem,
-      arrItem: this.state.arrItem.filter((item) => !item.isCompleted),
-    }));
-  };
-
-  showAllTask = (e) => {
-    let arr = this.state.arrItem;
-    console.log(arr);
-    this.setState((prevState) => ({
-      arrItem: [...(prevState.arrItem || [])],
     }));
   };
 
